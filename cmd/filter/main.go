481c13e80a03aaec6d27cfea74413ad39f3b4b9f
MuXiu1997 <muxiu1997@gmail.com>
2022-08-07 17:37:09 +0800

    Use ordered map to ensure the order of options

--- cmd/filter/main.go
+++ cmd/filter/main.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	aw "github.com/deanishe/awgo"
+	"github.com/elliotchance/orderedmap/v2"
 )
 
 var (
@@ -26,56 +27,58 @@ func run() {
 	expr = ".*" + expr + ".*"
 	compileExpr, _ := regexp.Compile(expr)
 
-	options := map[string]string{
-		"左半屏":  "leftHalf",
-		"右半屏":  "rightHalf",
-		"中间半屏": "centerHalf",
-		"上半屏":  "topHalf",
-		"下半屏":  "bottomHalf",
+	options := orderedmap.NewOrderedMap[string, string]()
 
-		"左上": "topLeft",
-		"右上": "topRight",
-		"左下": "bottomLeft",
-		"右下": "bottomRight",
+	options.Set("左半屏", "leftHalf")
+	options.Set("右半屏", "rightHalf")
+	options.Set("中间半屏", "centerHalf")
+	options.Set("上半屏", "topHalf")
+	options.Set("下半屏", "bottomHalf")
 
-		"最大化":   "maximize",
-		"接近最大化": "almostMaximize",
-		"最大化高度": "maximizeHeight",
-		"缩小":    "smaller",
-		"放大":    "larger",
-		"中央":    "center",
-		"恢复":    "restore",
+	options.Set("左上", "topLeft")
+	options.Set("右上", "topRight")
+	options.Set("左下", "bottomLeft")
+	options.Set("右下", "bottomRight")
 
-		"下一个显示器": "nextDisplay",
-		"上ー个显示器": "previousDisplay",
+	options.Set("最大化", "maximize")
+	options.Set("接近最大化", "almostMaximize")
+	options.Set("最大化高度", "maximizeHeight")
+	options.Set("缩小", "smaller")
+	options.Set("放大", "larger")
+	options.Set("中央", "center")
+	options.Set("恢复", "restore")
 
-		"前1/3":  "firstThird",
-		"中间1/3": "centerThird",
-		"后1/3":  "lastThird",
-		"前2/3":  "firstTwoThirds",
-		"后2/3":  "lastTwoThirds",
+	options.Set("下一个显示器", "nextDisplay")
+	options.Set("上ー个显示器", "previousDisplay")
 
-		"左首1/4": "firstFourth",
-		"左二1/4": "secondFourth",
-		"右二1/4": "thirdFourth",
-		"右首1/4": "lastFourth",
-		"左侧3/4": "firstThreeFourths",
-		"右侧3/4": "lastThreeFourths",
+	options.Set("前1/3", "firstThird")
+	options.Set("中间1/3", "centerThird")
+	options.Set("后1/3", "lastThird")
+	options.Set("前2/3", "firstTwoThirds")
+	options.Set("后2/3", "lastTwoThirds")
 
-		"向左移动": "moveLeft",
-		"向右移动": "moveRight",
-		"向上移动": "moveUp",
-		"向下移动": "moveDown",
+	options.Set("左首1/4", "firstFourth")
+	options.Set("左二1/4", "secondFourth")
+	options.Set("右二1/4", "thirdFourth")
+	options.Set("右首1/4", "lastFourth")
+	options.Set("左侧3/4", "firstThreeFourths")
+	options.Set("右侧3/4", "lastThreeFourths")
 
-		"左上1/6": "topLeftSixth",
-		"中上1/6": "topCenterSixth",
-		"右上1/6": "topRightSixth",
-		"左下1/6": "bottomLeftSixth",
-		"中下1/6": "bottomCenterSixth",
-		"右下1/6": "bottomRightSixth",
-	}
+	options.Set("向左移动", "moveLeft")
+	options.Set("向右移动", "moveRight")
+	options.Set("向上移动", "moveUp")
+	options.Set("向下移动", "moveDown")
 
-	for optionZH, optionEN := range options {
+	options.Set("左上1/6", "topLeftSixth")
+	options.Set("中上1/6", "topCenterSixth")
+	options.Set("右上1/6", "topRightSixth")
+	options.Set("左下1/6", "bottomLeftSixth")
+	options.Set("中下1/6", "bottomCenterSixth")
+	options.Set("右下1/6", "bottomRightSixth")
+
+	for option := options.Front(); option != nil; option = option.Next() {
+		optionZH := option.Key
+		optionEN := option.Value
 		pinyinKey := toPinyinKey(optionZH)
 		if compileExpr.MatchString(optionZH) || compileExpr.MatchString(pinyinKey) {
 			item := wf.NewItem(fmt.Sprintf("Rectangle - %s", optionZH))
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/deanishe/awgo v0.29.1
+	github.com/elliotchance/orderedmap/v2 v2.0.1
 	github.com/mozillazg/go-pinyin v0.19.0
 )
 
@@ -11,5 +12,6 @@ require (
 	github.com/magefile/mage v1.11.0 // indirect
 	go.deanishe.net/env v0.5.1 // indirect
 	go.deanishe.net/fuzzy v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20220321173239-a90fa8a75705 // indirect
 	golang.org/x/text v0.3.6 // indirect
 )
